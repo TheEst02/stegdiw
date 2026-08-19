fd43b7b96816b6390a3113759c6c70157c2d7c26
TheEst02 <theestablishingshot@gmail.com>
2026-08-19 14:14:17 +0100

    Update RSS feed URL to new repository

--- generate-rss.js
+++ generate-rss.js
@@ -7,7 +7,7 @@ const feed = new RSS({
   title: "FDA – This Week’s Releases",
   description: "Custom RSS feed generated from FDA release schedule",
   feed_url:
-    "https://raw.githubusercontent.com/YOURNAME/fda-this-week-feed/main/rss.xml",
+    "https://raw.githubusercontent.com/TheEst02/fda-this-week-feed/main/rss.xml",
   site_url:
     "https://filmdistributorsassociation.com/release-schedule/this-weeks-releases/",
   language: "en"
