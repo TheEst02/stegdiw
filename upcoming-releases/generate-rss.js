7380577a9661f9afc9302bfcb05edb516c4f4929
TheEst02 <theestablishingshot@gmail.com>
2026-08-19 14:38:52 +0100

    Update RSS feed URL for upcoming releases

--- upcoming-releases/generate-rss.js
+++ upcoming-releases/generate-rss.js
@@ -7,7 +7,7 @@ const feed = new RSS({
   title: "FDA – This Week’s Releases",
   description: "Custom RSS feed generated from FDA release schedule",
   feed_url:
-    "https://raw.githubusercontent.com/TheEst02/stegdiw/main/fda-this-week-feed/main/rss.xml",
+    "https://raw.githubusercontent.com/TheEst02/stegdiw/main/upcoming-releases/rss.xml",
   site_url:
     "https://filmdistributorsassociation.com/release-schedule/this-weeks-releases/",
   language: "en"
